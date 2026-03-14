d9dfe46f600a9546796fe2cb7117bc092510a57b
handahyun <dh.han414@gmail.com>
2026-03-14 19:07:34 +0900

    reverse print

--- Q1/main.py
+++ Q1/main.py
@@ -2,10 +2,24 @@
 print('Hello Mars')
 
 # 로그 출력
+# 전체 읽기
 try:
     with open('mission_computer_main.log', 'r', encoding='utf-8') as file:
-        for line in file:
+        lines = file.readlines()
+
+        # 출력
+        for line in lines:
             print(line, end='')
+
+        print('---')
+
+        # 역순 출력
+        for line in reversed(lines):
+            print(line, end='')
+
 # 예외 처리
 except Exception as error:
     print(f'오류 발생 : {error}')
+
+
+    
