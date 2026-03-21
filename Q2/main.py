e7c97b658d2277393bb5ff584cc274f66972a914
handahyun <dh.han414@gmail.com>
2026-03-22 00:16:44 +0900

    Save and read binary file

--- Q2/main.py
+++ Q2/main.py
@@ -1,7 +1,10 @@
 import csv
+# 데이터를 오래 보관할 수 있는 이진 형태로 절이기(pickling)
+import pickle
+
 
 try:
-    #list 객체로 변환
+    # list 객체로 변환
     with open('Mars_Base_Inventory_List.csv', 'r', encoding='utf-8') as file:
         reader = csv.DictReader(file)
         
@@ -9,25 +12,44 @@ try:
         #print('리스트 객체로 변환', data)
 
 
-#람다식과 동일
+# 람다식과 동일
 # def get_flammability(x):
-#    return float(x['Flammability'])
+#     return float(x['Flammability'])
 # key=get_flammability
 
     # Flammability 높은 순서대로 정렬
     sorted_data = sorted(data, key=lambda x: float(x['Flammability']), reverse=True)
 
+
     # Flammability 0.7 이상 출력
     danger_data = [item for item in sorted_data if float(item['Flammability']) >= 0.7]
+    print('---0.7 이상 출력---')
     for item in danger_data:
         print(item)
 
-# CSV로 저장
+
+    # CSV로 저장
     with open('Mars_Base_Inventory_danger.csv', 'w', encoding='utf-8', newline='') as file:
         fieldnames = data[0].keys()
         writer = csv.DictWriter(file, fieldnames=fieldnames)
         writer.writeheader()
         writer.writerows(danger_data)
+
+
+    # Flammability 높은 순서대로 정렬된 목록을 이진 파일로 저장
+    # wb : 이진 파일 쓰기 모드
+    with open('Mars_Base_Inventory_List.bin', 'wb') as file:
+        pickle.dump(sorted_data, file)
+
+
+    # Mars_Base_Inventory_List.bin 파일 출력
+    # wb : 이진 파일 읽기 모드
+    with open('Mars_Base_Inventory_List.bin', 'rb') as file:
+        loaded_data = pickle.load(file)
+    print('---이진 파일 출력---')
+    for item in loaded_data:
+        print(item)
+
 
 # 예외 처리
 except Exception as error:
