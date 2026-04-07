39038a957d12822a58efe854064004200194416d
handahyun <dh.han414@gmail.com>
2026-04-07 19:14:14 +0900

    Add a 5-minute average

--- Q4/mars_mission_computer.py
+++ Q4/mars_mission_computer.py
@@ -23,22 +23,53 @@ class MissionComputer:
 
     def get_sensor_data(self):
 
-        # 강제로 종료(Ctrl+C)하지 않는 한 무한 반복
-        while True:
+        # 수집한 값들을 저장할 리스트
+        history = []
+        # 반복 횟수 카운트
+        count = 0
 
-            # Q3.DummySensor의 set_env()를 호출하여 Q3의 _env_values에 랜덤값 채움
-            self.ds.set_env()
+        try:
+            # 강제로 종료(Ctrl+C)하지 않는 한 무한 반복
+            while True:
 
-            # Q3.DummySensor의 get_env()를 호출하여 _env_values에 채워진 값을 받아와 Q4.MissionComputer의 env_values에 저장
-            self.env_values = self.ds.get_env()
+                # Q3.DummySensor의 set_env()를 호출하여 Q3의 _env_values에 랜덤값 채움
+                self.ds.set_env()
 
-            # env_values를 JSON 형태로 변환해서 출력
-            # 4칸 들여쓰기
-            print(json.dumps(self.env_values, indent=4))
+                # Q3.DummySensor의 get_env()를 호출하여 _env_values에 채워진 값을 받아와 Q4.MissionComputer의 env_values에 저장
+                self.env_values = self.ds.get_env()
 
-            # 5초마다 반복
-            time.sleep(5)
+                # env_values를 JSON 형태로 변환해서 출력
+                # 4칸 들여쓰기
+                print(json.dumps(self.env_values, indent=4))
 
+
+                # 매 5초마다 값을 history에 저장
+                history.append(self.env_values.copy())
+                count += 1
+            
+
+                # 60회(5분)마다 평균 출력
+                if count == 60:
+                    # 평균값이 담길 dict
+                    average = {}
+
+                    for key in self.env_values:
+                        # []에 담긴 60개 값을 키 별로 더한 후 60으로 나눔, 소숫점 2의 자리까지 표현
+                        average[key] = round(sum(d[key] for d in history) / 60, 2)
+                    print('--- 5분 평균 ---')
+                    print(json.dumps(average, indent=4))
+
+                    # history와 count 초기화
+                    history = []
+                    count = 0
+
+                # 5초마다 반복
+                time.sleep(5)
+
+
+        # ctrl+c 
+        except KeyboardInterrupt:
+            print('System stopped....')
 
 # MissionComputer를 RunComputer라는 이름으로 인스턴스화
 RunComputer = MissionComputer()
